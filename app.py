b0fdbc0a7fa4df14f8b7092e8c89b0c1eee8273c
anoldlunaxx <160794203+anoldlunaxx@users.noreply.github.com>
2025-10-18 14:08:39 -0500

    Shorten RECAPTCHA and TURNSTILE secret keys

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from flask import Flask, request, redirect, send_file, jsonify
 
 app = Flask(__name__)
 
-RECAPTCHA_SECRET = os.environ.get("RECAPTCHA_SECRET", "6Ldi8u4rAAAAAJCiOkZzSCUt0to0ca1cYinnA_gh")
-TURNSTILE_SECRET = os.environ.get("TURNSTILE_SECRET", "0x4AAAAAAB7Wt6wXDqvBb9oJAT9EtvHvSKA")
+RECAPTCHA_SECRET = os.environ.get("RECAPTCHA_SECRET", "6Ldi8u4rAAAAAJCiOkZzSCUt0t")
+TURNSTILE_SECRET = os.environ.get("TURNSTILE_SECRET", "0x4AAAAAAB7Wt6wXDqvBb9oJ")
 
 @app.route("/")
 def index():
