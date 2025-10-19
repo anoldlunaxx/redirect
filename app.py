6286c87acaf8f401fdefb34b80bf366e9ac5d281
anoldlunaxx <160794203+anoldlunaxx@users.noreply.github.com>
2025-10-19 07:40:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, request, redirect, send_file, jsonify
 
 app = Flask(__name__)
 
-RECAPTCHA_SECRET = os.environ.get("RECAPTCHA_SECRET", "6Ldmm-8rAAAAAEMtc5gFW46XtdA0slD8pE14EnkC")
+RECAPTCHA_SECRET = os.environ.get("RECAPTCHA_SECRET", "6Lcune8rAAAAAN8ICsV6M7W9Zm_p4MWsPFmoq1VK")
 TURNSTILE_SECRET = os.environ.get("TURNSTILE_SECRET", "0x4AAAAAAB7cGXh4GEgIOyDOwsgtH-7QEsI")
 
 @app.route("/")
